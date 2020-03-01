476c30e3fb340e3e40ed830488a56ed24237c475
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-02 01:53:18 +0530

    selector class

---
+++ hello-world/src/app/components/selectorclass.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: ".app-selector-class",
+  templateUrl: "./selectorclass.component.html",
+  styleUrls: ["./selectorclass.component.css"]
+})
+export class SelectorClassComponent implements OnInit {
+  //title = "Hello World";
+
+  //constructor() {}
+
+  ngOnInit() {}
+}
