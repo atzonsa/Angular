92375332ebd4e6242ec48cb3546eb260307939f8
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-02 01:53:33 +0530

    selector attribute

---
+++ hello-world/src/app/components/selectorattribute.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "[app-selector-attribute]",
+  templateUrl: "./selectorattribute.component.html",
+  styleUrls: ["./selectorattribute.component.css"]
+})
+export class SelectorAttributeComponent implements OnInit {
+  //title = "Hello World";
+
+  //constructor() {}
+
+  ngOnInit() {}
+}
