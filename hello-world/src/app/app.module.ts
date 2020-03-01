4291312678dbb59e0e7ebbcddbcf3fdb563431ed
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-02 01:53:57 +0530

    component added

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,8 +3,16 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 
+import { TestComponent } from "./components/test.component";
+import { SelectorClassComponent } from "./components/selectorclass.component";
+import { SelectorAttributeComponent } from "./components/selectorattribute.component";
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    TestComponent,
+    SelectorClassComponent,
+    SelectorAttributeComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
